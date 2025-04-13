4e68b6eac500b839a9ed24ec1599b01a47d28a21
G-Dillep <gdillepreddy@gmail.com>
2025-04-13 19:31:55 +0530

    created templates folder for doctor

--- doctor/views.py
+++ doctor/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def home(request):
-    return HttpResponse("Hello, Doctor!")
+    return render(request, 'doctor/home.html')
