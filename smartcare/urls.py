2c160cad7ed285b46ac764e6a8bd6815eb84a935
G-Dillep <gdillepreddy@gmail.com>
2025-04-13 23:44:10 +0530

    completed project structure almost

--- smartcare/urls.py
+++ smartcare/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('patient/', include('patient.urls')),
     path('doctor/', include('doctor.urls')),
     path('hospital/', include('hospital.urls')),
+    path('', views.home, name='home'),
 ]
